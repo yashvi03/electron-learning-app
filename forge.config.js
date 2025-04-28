d84663cb51a962bcef270508bfd4830042fd61e1
yashvi03 <yashviagrawal03@gmail.com>
2025-04-28 14:34:18 +0530

    github token issue

--- forge.config.js
+++ forge.config.js
@@ -1,5 +1,7 @@
 const { FusesPlugin } = require("@electron-forge/plugin-fuses");
 const { FuseV1Options, FuseVersion } = require("@electron/fuses");
+require('dotenv').config();
+
 
 module.exports = {
   packagerConfig: {
